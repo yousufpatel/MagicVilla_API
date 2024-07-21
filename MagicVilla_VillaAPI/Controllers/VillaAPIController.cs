ea395c7151aeca9c0ffec6dc9f3182265c52a3b9
yousuf.glb <yousuf.glb@gmail.com>
2024-07-21 18:20:41 +0530

    Refactor GetVilla function for improved readability and consistency.

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Data;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    //[Route("api/[Controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.villaList());
+        }
+
+        [HttpGet("id:int")]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList().FirstOrDefault(v => v.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,16 @@
+﻿using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList()
+        {
+            return new List<VillaDTO>()
+            {
+                new VillaDTO(){ Id=1, Name="Pool View"},
+                new VillaDTO(){ Id=2, Name="Beach View"}
+            };
+        }
+    }
+}
